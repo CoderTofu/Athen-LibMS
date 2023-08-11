8bcacd82b781bc8c4d8d1e85d43b9d051c66cbd8
CoderTofu <vioneverforget@gmail.com>
2023-08-11 21:42:07 +0800

    ✨ Created the basic app layout 🖌️

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import classes.*;
 public class Main {
     public static void main(String[] args) {
         Library myLib = new Library();
+        new MyFrame();
     }
 
 //    IMPORTANT STUFF:
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,9 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame{
     MyFrame() {
         this.setVisible(true);
-        this.setSize(500, 500);
+        this.setSize(1000, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        JPanel commandPanel = new JPanel();
+        commandPanel.setBounds(0, 0, 283, 700);
+        commandPanel.setBackground(new Color(77, 218, 227));
+
+        JPanel titlesPanel = new JPanel();
+        titlesPanel.setBounds(283, 0, 717, 100);
+        titlesPanel.setBackground(new Color(47, 155, 255));
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBounds(283, 100, 717, 600);
+        contentPanel.setBackground(Color.lightGray);
+
+        this.add(commandPanel);
+        this.add(titlesPanel);
+        this.add(contentPanel);
     }
 }
